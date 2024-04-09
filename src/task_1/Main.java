de480c5dbcf3d8d9a08b35559f4e37699c11c3c3
arai25 <araibidanova@mail.ru>
2024-04-09 21:11:57 +0500

    This method finds the minimum element in the array.

--- src/task_1/Main.java
+++ src/task_1/Main.java
@@ -3,27 +3,26 @@ package task_1;
 public class Main {
     public static void main(String[] args) {
         double startTime = System.nanoTime();
-        int n = 5;
-        int[] arr = new int[]{10, 1, 32, 3, 45};
-        int r = fun(n, arr);
+        int[] arr = {10, 1, 32, 3, 45};
+        int min = f(arr, arr.length);
         double endTime = System.nanoTime();
-        double duration = (endTime - startTime) / 1000000;
-        System.out.println(r);
+        double duration = (endTime-startTime) / 1000000; // Convert nanoseconds to milliseconds
+        System.out.println(min);
         System.out.println("Time taken: " + duration + " milliseconds");
-
     }
     /*
-     * This method finds the minimum value among the elements of the array
-     * It takes given int n and an array
-     * Time complexity: O(n), where n is the input number
-     * Linear time complexity
+    * This method finds the minimum element in the array.
+    * It uses a recursive approach.
+    * Time complexity: O(n), where n is the input number.
+    * Linear time complexity.
+    *
+    * @param arr The array of integers.
+    * @param n The number of elements in the array.
+    * @return The minimum element in the array.
      */
-    public static int fun(int n, int[] arr) {
-        int min = arr[0];
-        for (int i = 0; i < n; i++) {
-            if (arr[i] < min)
-                min = arr[i];
-        }
-        return min;
+    public static int f(int[] arr, int n) {
+        if (n == 1)
+            return arr[0];
+        return Math.min(arr[n - 1], f(arr, n - 1));
     }
 }
